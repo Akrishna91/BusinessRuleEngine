529132a296d168d78d41c32313be297b21c3411d
kbaghel <kumar.baghel@walmart.com>
2016-02-29 12:19:21 +0530

    Updating name to represent implementation

---
+++ BusinessRuleEngine/src/com/java/main/businessrules/AssociationBusinessRulesImpl.java
@@ -0,0 +1,15 @@
+package com.java.main.businessrules;
+
+/**
+ * Class responsible to generate business rules from AssociationRulesW
+ * @author Karthik
+ *
+ */
+public class AssociationBusinessRulesImpl {
+	
+	public void test(){
+		System.out.println("test");
+		
+	}
+
+}
