ec04d754d4003553c15b3d648c6ebedc6a6f73d9
atripa5 <azad.tripathi@walmart.com>
2016-02-29 14:39:22 +0530

    Change in businesrules classes

---
+++ BusinessRuleEngine/src/com/java/main/businessrules/CardinalBusinessRulesImpl.java
@@ -0,0 +1,10 @@
+package com.java.main.businessrules;
+
+/**
+ * Class responsible for generating business rules based on cardinality
+ * @author Karthik
+ *
+ */
+public class CardinalBusinessRulesImpl {
+
+}
---
+++ BusinessRuleEngine/src/com/java/main/businessrules/DistinctValueBusinessRulesImpl.java
@@ -0,0 +1,10 @@
+package com.java.main.businessrules;
+
+/**
+ * Class which is responsible for generating business rules based on distinct values on each attribute
+ * @author Karthik
+ *
+ */
+public class DistinctValueBusinessRulesImpl {
+
+}
