e9610de8bd5d88a9f48ad979425186a005873670
atripa5 <azad.tripathi@walmart.com>
2016-02-29 15:19:41 +0530

    test123

--- BusinessRuleEngine/src/com/java/main/businessrules/BusinessRule.java
+++ BusinessRuleEngine/src/com/java/main/businessrules/BusinessRule.java
@@ -3,8 +3,8 @@ package com.java.main.businessrules;
 /**
  * Abstract class which is responsible for holding the business rule generate logic. This class will implement interface IBusinessRule which will override generateRules()
  * @see AssociationBusinessRulesImpl for generating business rules from Association Rules
- * @see CardinalBusinessRules for generating business rules from Cardinality
- * @see DistinctValueBusinessRules for generating business rules based on distinct values available for each attribute
+ * @see CardinalBusinessRulesImpl for generating business rules from Cardinality
+ * @see DistinctValueBusinessRulesImpl for generating business rules based on distinct values available for each attribute
  * @see PossibleValuesBusinessRules for generating business rules based on distinct values available for each attribute
  * @author kkaruna
  *
